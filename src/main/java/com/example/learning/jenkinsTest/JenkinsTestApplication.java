6ab237b15dde37215abce7b948d3975ac66568e7
soulxseeksgg <pontree.kongjanta@gmail.com>
2025-07-28 07:45:55 +0700

    hello jenkins

---
+++ src/main/java/com/example/learning/jenkinsTest/JenkinsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.learning.jenkinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+		System.out.println("hello myapp");
+	}
+
+}
---
+++ src/test/java/com/example/learning/jenkinsTest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning.jenkinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
